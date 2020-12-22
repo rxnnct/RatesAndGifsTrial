f00052e8b8882f1637f6452db16500fefe340182
Rixen <rixennoct@rambler.ru>
2020-12-22 19:10:24 +0300

    Rates client.

--- src/main/java/ru/rxnnct/RatesAndGifsTrial/RatesAndGifsTrialApplication.java
+++ src/main/java/ru/rxnnct/RatesAndGifsTrial/RatesAndGifsTrialApplication.java
@@ -2,9 +2,14 @@ package ru.rxnnct.RatesAndGifsTrial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RatesAndGifsTrialApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RatesAndGifsTrialApplication.class, args);
