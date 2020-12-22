33bd4be58a4fc990ebe9aff04c7fa85088c64ef3
Rixen <rixennoct@rambler.ru>
2020-12-22 12:37:30 +0300

    Initial commit

---
+++ src/main/java/ru/rxnnct/RatesAndGifsTrial/RatesAndGifsTrialApplication.java
@@ -0,0 +1,13 @@
+package ru.rxnnct.RatesAndGifsTrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatesAndGifsTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatesAndGifsTrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rxnnct/RatesAndGifsTrial/RatesAndGifsTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rxnnct.RatesAndGifsTrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatesAndGifsTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
